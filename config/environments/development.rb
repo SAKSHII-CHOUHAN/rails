4de23adfb76fd32f8a8517e9a8a131f68b8dc09d
SAKSHII-CHOUHAN <96692556+SAKSHII-CHOUHAN@users.noreply.github.com>
2025-02-14 19:13:46 +0530

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,10 +33,10 @@ Rails.application.configure do
   
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => "sakshichouhan016",
-    :password             => "hzzq hgou sahu oqip",
+    :address              => "",
+    :port                 => ,
+    :user_name            => "",
+    :password             => "",
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
